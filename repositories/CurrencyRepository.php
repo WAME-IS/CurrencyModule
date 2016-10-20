ddd8872af6f4028604bdda438e1574a63874c150
Stanislav Červeňák <stano@wame.sk>
2016-10-20 16:21:57 +0200

    ADD getCurrencyList() method in currencyRepository

--- repositories/CurrencyRepository.php
+++ repositories/CurrencyRepository.php
@@ -5,16 +5,17 @@ namespace Wame\CurrencyModule\Repositories;
 use Wame\Core\Repositories\BaseRepository;
 use Wame\CurrencyModule\Entities\CurrencyEntity;
 
+
 class CurrencyRepository extends BaseRepository
 {
 	const STATUS_REMOVE = 0;
 	const STATUS_ENABLED = 1;
 	const STATUS_DISABLED = 2;
-    
+
 	const MAIN_DISABLED = 0;
 	const MAIN_ENABLED = 1;
 
-	
+
     public function __construct()
     {
         parent::__construct(CurrencyEntity::class);
@@ -23,7 +24,7 @@ class CurrencyRepository extends BaseRepository
 
 	/**
 	 * Update currency
-	 * 
+	 *
 	 * @param CurrencyEntity $currencyEntity
 	 * @return CurrencyEntity
 	 */
@@ -31,5 +32,24 @@ class CurrencyRepository extends BaseRepository
 	{
 		return $currencyEntity;
 	}
+
+
+    /**
+     * Get currency list
+     *
+     * @return array
+     */
+    public function getCurrencyList()
+    {
+        $return = [];
+
+		$currencyEntity = $this->find([], ['code' => 'ASC']);
+
+		foreach ($currencyEntity as $currency) {
+			$return[$currency->getId()] = $currency->getCode() . ' - ' . $currency->getTitle() . ' - ' . $currency->getSymbol();
+		}
+
+		return $return;
+    }
 
 }
